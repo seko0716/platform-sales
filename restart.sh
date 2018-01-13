d6c5504c8b74069f6038fb9ad5b22eb76cf4e9dc
sergey <k.s.34@yande.ru>
2018-01-13 19:18:41 +0400

    починил доступ к базе, не понимаю почему не работает с заданием пароля. пока так как база не видна вне контейнера оставлю без пароля

--- restart.sh
+++ restart.sh
@@ -3,6 +3,6 @@
 export CONFIG_SERVICE_PASSWORD=111
 export MONGODB_PASSWORD=222
 export COMMUNICATION_SERVICE_PASSWORD=333
-export MONGO_INITDB_DATABASE=piggymetrics
+export MONGO_INITDB_DATABASE=authdb
 
 ./stop.sh ; ./docker_build.sh ; ./start.sh ; ./status.sh
