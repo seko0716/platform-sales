36d7e13aa961eb08a44f707f85fdc50b3464756c
seko0716 <sergey3440@gmail.com>
2018-01-16 10:56:37 +0400

    add order and config for date in db and json

---
+++ buisness/market/src/main/kotlin/net/sergey/kosov/market/domains/Order.kt
@@ -0,0 +1,18 @@
+package net.sergey.kosov.market.domains
+
+import com.fasterxml.jackson.annotation.JsonFormat
+import org.bson.types.ObjectId
+import org.springframework.data.annotation.Id
+import org.springframework.data.mongodb.core.mapping.Document
+import java.time.LocalDateTime
+
+@Document(collection = "orders")
+data class Order(@Id var id: ObjectId = ObjectId(),
+                 var goods: Goods,
+                 @JsonFormat(pattern = "dd-MM-yyyy hh:mm:ss")
+                 var createdTime: LocalDateTime = LocalDateTime.now(),
+                 var status: Status = Status.created,
+                 @JsonFormat(pattern = "dd-MM-yyyy hh:mm:ss")
+                 var submittedTime: LocalDateTime? = null) {
+
+}
---
+++ buisness/market/src/main/kotlin/net/sergey/kosov/market/domains/Status.java
@@ -0,0 +1,5 @@
+package net.sergey.kosov.market.domains;
+
+public enum Status {
+    created;
+}
