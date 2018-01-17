c5019f4ada3dd8f16ac89d9a26dbe687e21558e4
seko0716 <sergey3440@gmail.com>
2018-01-17 11:40:22 +0400

    add repositories

---
+++ buisness/market/src/main/kotlin/net/sergey/kosov/market/repository/CategoryRepository.kt
@@ -0,0 +1,8 @@
+package net.sergey.kosov.market.repository
+
+import net.sergey.kosov.market.domains.Category
+import org.bson.types.ObjectId
+import org.springframework.data.mongodb.repository.MongoRepository
+
+
+interface CategoryRepository : MongoRepository<Category, ObjectId>
--- buisness/market/src/main/kotlin/net/sergey/kosov/market/repository/OrderRepository.kt
+++ buisness/market/src/main/kotlin/net/sergey/kosov/market/repository/OrderRepository.kt
@@ -1,9 +1,9 @@
 package net.sergey.kosov.market.repository
 
-import net.sergey.kosov.market.domains.Goods
+import net.sergey.kosov.market.domains.Order
 import org.bson.types.ObjectId
 import org.springframework.data.mongodb.repository.MongoRepository
 import org.springframework.stereotype.Repository
 
 @Repository
-interface GoodsRepository : MongoRepository<Goods, ObjectId>
+interface OrderRepository : MongoRepository<Order, ObjectId>
