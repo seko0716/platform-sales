bd0ae87bf575ae858b54d48ca152bc0b6952ff74
seko0716 <sergey3440@gmail.com>
2018-01-17 11:45:18 +0400

    refactoring

--- buisness/market/src/main/kotlin/net/sergey/kosov/market/MarketApplication.kt
+++ buisness/market/src/main/kotlin/net/sergey/kosov/market/MarketApplication.kt
@@ -2,11 +2,8 @@ package net.sergey.kosov.market
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.autoconfigure.domain.EntityScan
-import org.springframework.data.convert.Jsr310Converters
 
 @SpringBootApplication
-@EntityScan(value = ["net.sergey.kosov.market.domains"], basePackageClasses = [Jsr310Converters::class])
 class MarketApplication
 
 fun main(args: Array<String>) {
---
+++ buisness/market/src/main/kotlin/net/sergey/kosov/market/configuration/MarketConfiguration.kt
@@ -0,0 +1,9 @@
+package net.sergey.kosov.market.configuration
+
+import org.springframework.boot.autoconfigure.domain.EntityScan
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.convert.Jsr310Converters
+
+@Configuration
+@EntityScan(value = ["net.sergey.kosov.market.domains"], basePackageClasses = [Jsr310Converters::class])
+class MarketConfiguration {}
