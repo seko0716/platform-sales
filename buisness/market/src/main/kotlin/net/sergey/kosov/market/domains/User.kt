aafb2dfe7926df47f38d01dcffecf971a5123308
seko0716 <sergey3440@gmail.com>
2018-01-17 11:43:24 +0400

    refactoring

--- buisness/market/src/main/kotlin/net/sergey/kosov/market/domains/User.kt
+++ buisness/market/src/main/kotlin/net/sergey/kosov/market/domains/User.kt
@@ -2,6 +2,4 @@ package net.sergey.kosov.market.domains
 
 data class User(var name: String,
                 var family: String,
-                var fullName: String = "$name $family") {
-
-}
+                var fullName: String = "$name $family")
--- buisness/market/src/main/kotlin/net/sergey/kosov/market/utils/Extinctions.kt
+++ buisness/market/src/main/kotlin/net/sergey/kosov/market/utils/Extinctions.kt
@@ -2,6 +2,5 @@ package net.sergey.kosov.market.utils
 
 import org.bson.types.ObjectId
 
-
 fun String.toObjectId(): ObjectId = ObjectId(this)
 
