97ea05a7afa891dc3f12165f490a3f3bb33da91e
Sergey34 <sergey3440@gmail.com>
2018-01-20 14:59:49 +0400

    fixed test in config service

--- buisness/statistic-service/src/test/kotlin/net/sergey/kosov/statistic/TestContext.kt
+++ buisness/statistic-service/src/test/kotlin/net/sergey/kosov/statistic/TestContext.kt
@@ -1,10 +1,15 @@
 package net.sergey.kosov.statistic
 
-//@RunWith(SpringRunner::class)
-//@SpringBootTest
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
 class TestContext {
-//    @Test
-//    fun contextLoads() {
-//
-//    }
+    @Test
+    fun contextLoads() {
+
+    }
 }
