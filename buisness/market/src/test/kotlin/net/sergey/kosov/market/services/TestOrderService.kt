c633cfd991836670113f6c76b52732c36984f762
Sergey34 <sergey3440@gmail.com>
2018-01-25 18:15:53 +0400

    fixed tests context

--- buisness/market/src/test/kotlin/net/sergey/kosov/market/services/TestOrderService.kt
+++ buisness/market/src/test/kotlin/net/sergey/kosov/market/services/TestOrderService.kt
@@ -1,5 +1,6 @@
 package net.sergey.kosov.market.services
 
+import net.sergey.kosov.market.api.StatisticApi
 import net.sergey.kosov.market.configuration.ConfigurationFeign
 import net.sergey.kosov.market.domains.Order
 import net.sergey.kosov.market.domains.Status
@@ -7,8 +8,10 @@ import net.sergey.kosov.market.domains.User
 import org.bson.types.ObjectId
 import org.junit.Assert
 import org.junit.Before
+import org.junit.Test
 import org.junit.runner.RunWith
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.mock.mockito.MockBean
 import org.springframework.test.context.ContextConfiguration
 import org.springframework.test.context.ContextHierarchy
 import org.springframework.test.context.junit4.SpringRunner
@@ -20,6 +23,8 @@ class TestOrderService {
     lateinit var orderService: OrderService
     @Autowired
     lateinit var goodsService: GoodsService
+    @MockBean
+    private lateinit var statisticService: StatisticApi
 
     var orderId = ObjectId()
 
@@ -30,6 +35,7 @@ class TestOrderService {
         orderId = order.id
     }
 
+    @Test
     fun createNewOrder() {
         val goods = goodsService.createGoods(title = "name", description = "description")
         var currentUser = User("2", "3")
@@ -38,24 +44,28 @@ class TestOrderService {
         Assert.assertEquals(Status.CREATED, order.status)
     }
 
+    @Test
     fun processOrder() {//после оплаты
         var order: Order = orderService.findOrder(orderId = orderId)
         val processedOrder = orderService.processOrder(order = order)
         Assert.assertEquals(Status.PROCESSING, processedOrder.status)
     }
 
+    @Test
     fun completeOrder() {//после оплаты
         var order: Order = orderService.findOrder(orderId = orderId)
         val completedOrder = orderService.completeOrder(order = order)
         Assert.assertEquals(Status.COMPLETED, completedOrder.status)
     }
 
+    @Test
     fun cancelOrder() {//после оплаты
         var order: Order = orderService.findOrder(orderId = orderId)
         var canceledOrder: Order = orderService.cancelOrder(order = order)
         Assert.assertEquals(Status.CANCELED, canceledOrder.status)
     }
 
+    @Test
     fun getOrders() {
         val ordersExp = (0..10).mapTo(ArrayList()) {
             val goods = goodsService.createGoods(title = "$it", description = "description!!$it")
