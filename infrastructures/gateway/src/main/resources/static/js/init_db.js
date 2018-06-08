136ea3bd17e860553ee7e45ed6e93af3ee4b3959
Sergey34 <sergey3440@gmail.com>
2018-06-08 18:44:25 +0400

    delete old artifacts

--- infrastructures/gateway/src/main/resources/static/js/init_db.js
+++
@@ -1,9 +0,0 @@
-function updateDB() {
-    $(document).ready(function () {
-        $.ajax({
-            url: rootUrl + "/rest/write/init"
-        }).then(function (data) {
-            console.log(data);
-        });
-    });
-}
