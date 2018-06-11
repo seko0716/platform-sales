fa814a51ba2bbcbf301e5cc6aad1900ea0197270
Sergey34 <sergey3440@gmail.com>
2018-06-11 16:29:06 +0400

    market with auth server

--- buisness/market/src/main/kotlin/net/sergey/kosov/market/MarketApplication.kt
+++ buisness/market/src/main/kotlin/net/sergey/kosov/market/MarketApplication.kt
@@ -2,9 +2,16 @@ package net.sergey.kosov.market
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer
 
+
+@EnableOAuth2Client
+@EnableResourceServer
 @SpringBootApplication
-class MarketApplication
+class MarketApplication {
+
+}
 
 fun main(args: Array<String>) {
     SpringApplication.run(MarketApplication::class.java, *args)
