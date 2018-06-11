ff8aaedaaaa0e0b889fd977134c1f91ec8d4e774
Sergey34 <sergey3440@gmail.com>
2018-06-11 13:03:23 +0400

    fixed message imports

--- buisness/communication/src/main/kotlin/net/sergey/kosov/communication/domains/Message.kt
+++ buisness/communication/src/main/kotlin/net/sergey/kosov/communication/domains/Message.kt
@@ -2,6 +2,7 @@ package net.sergey.kosov.communication.domains
 
 import com.fasterxml.jackson.databind.annotation.JsonSerialize
 import com.google.gson.Gson
+import net.sergey.kosov.common.serializers.ObjectIdSerializer
 import org.bson.types.ObjectId
 import org.springframework.data.annotation.Id
 import org.springframework.data.mongodb.core.mapping.Document
@@ -15,8 +16,8 @@ data class Message(@Id @JsonSerialize(using = ObjectIdSerializer::class) var id:
                    var from: String = "",
                    var accessToken: String = "",
                    var status: Status = Status.CREATED,
-                   var orderId: ObjectId? = null,
-                   var goodsId: ObjectId? = null,
+                   @JsonSerialize(using = ObjectIdSerializer::class) var orderId: ObjectId? = null,
+                   @JsonSerialize(using = ObjectIdSerializer::class) var goodsId: ObjectId? = null,
                    var creationDate: LocalDateTime = LocalDateTime.now(),
                    var completedDate: LocalDateTime? = null) {
     fun toJson(): String {
