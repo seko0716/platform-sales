cad61d7ec8e42d8c0a9c461e9d93f7e03dfaaec8
Sergey34 <sergey3440@gmail.com>
2018-06-22 23:44:37 +0400

    refactoring data load js

--- buisness/market/src/main/resources/static/js/data_load.js
+++ buisness/market/src/main/resources/static/js/data_load.js
@@ -1,31 +1,38 @@
+function _fillData(template, products, id) {
+    $(document).ready(function () {
+        $(id).html(Mustache.to_html(template, products));
+    });
+}
+
+function _fillProductList(products) {
+    var template =
+        "{{#.}}<div class=\"col-sm-4 col-lg-4 col-md-4\">" +
+        "<div class=\"thumbnail\">" +
+        "<img alt=\"\" src=\"http://placehold.it/320x150\">" +
+        "<div class=\"caption\">" +
+        "<h4 class=\"pull-right\">{{price}}</h4>" +
+        "<h4><a href=\"/market/view/product/{{id}}\">{{title}}</a></h4>" +
+        "<p>{{description}}</p>" +
+        "</div>" +
+        "<div class=\"ratings\">" +
+        "<p class=\"pull-right\">15 reviews</p>" +
+        "<p>" +
+        "<span class=\"glyphicon glyphicon-star\"></span>" +
+        "<span class=\"glyphicon glyphicon-star\"></span>" +
+        "<span class=\"glyphicon glyphicon-star\"></span>" +
+        "<span class=\"glyphicon glyphicon-star\"></span>" +
+        "<span class=\"glyphicon glyphicon-star\"></span>" +
+        "</p>" +
+        "</div>" +
+        "</div>" +
+        "</div>{{/.}}";
+
+    _fillData(template, products, "#products");
+}
+
 function loadProducts() {
-
     $.get("/market/products", function (products) {
-        var template =
-            "{{#.}}<div class=\"col-sm-4 col-lg-4 col-md-4\">" +
-            "<div class=\"thumbnail\">" +
-            "<img alt=\"\" src=\"http://placehold.it/320x150\">" +
-            "<div class=\"caption\">" +
-            "<h4 class=\"pull-right\">{{price}}</h4>" +
-            "<h4><a href=\"/market/view/product/{{id}}\">{{title}}</a></h4>" +
-            "<p>{{description}}</p>" +
-            "</div>" +
-            "<div class=\"ratings\">" +
-            "<p class=\"pull-right\">15 reviews</p>" +
-            "<p>" +
-            "<span class=\"glyphicon glyphicon-star\"></span>" +
-            "<span class=\"glyphicon glyphicon-star\"></span>" +
-            "<span class=\"glyphicon glyphicon-star\"></span>" +
-            "<span class=\"glyphicon glyphicon-star\"></span>" +
-            "<span class=\"glyphicon glyphicon-star\"></span>" +
-            "</p>" +
-            "</div>" +
-            "</div>" +
-            "</div>{{/.}}";
-
-        $(document).ready(function () {
-            $("#products").html(Mustache.to_html(template, products));
-        });
+        _fillProductList(products);
     });
 }
 
@@ -46,9 +53,7 @@ function loadChart() {
         if (products.length === 0) {
             document.getElementById('history_label').hidden = true
         }
-        $(document).ready(function () {
-            $("#history").html(Mustache.to_html(template, products));
-        });
+        _fillData(template, products, "#history");
     });
 }
 
@@ -61,5 +66,11 @@ function loadAvailableCategories() {
 function loadCategoryById(id) {
     $.get("/market/category/" + id, function (category) {
 
+    })
+}
+
+function loadProductsByShop(shopName) {
+    $.get("/market/products/market/" + shopName, function (products) {
+        _fillProductList(products)
     })
 }
