5d871325c059e07cece114fe13720c0bed5fa527
Sergey34 <sergey3440@gmail.com>
2018-06-23 01:02:47 +0400

    refactoring js

--- buisness/market/src/main/resources/static/js/data_load.js
+++ buisness/market/src/main/resources/static/js/data_load.js
@@ -74,20 +74,3 @@ function loadProductsByShop(shopName) {
         _fillProductList(products)
     })
 }
-
-function createCategory() {
-    $.ajax({
-        method: "PUT",
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-        },
-        url: "/market/category",
-        dataType: "json",
-        data: JSON.stringify({title: "title", description: "description", parentId: "parentId"})
-    }, function (category) {
-        console.log(category)
-    }).fail(function (error) {
-        alert("error");
-    });
-}
