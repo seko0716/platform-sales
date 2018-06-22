b99241c25e3abf0374ebc63008938a46b29d3b7a
Sergey34 <sergey3440@gmail.com>
2018-06-22 23:10:07 +0400

    add endpoints to view controller

--- buisness/market/src/main/kotlin/net/sergey/kosov/market/controllers/view/ViewController.kt
+++ buisness/market/src/main/kotlin/net/sergey/kosov/market/controllers/view/ViewController.kt
@@ -10,4 +10,25 @@ class ViewController {
     fun products(): String {
         return "products"
     }
+
+    @GetMapping("/view/product")
+    fun product(): String {
+        return "product"
+    }
+
+    @GetMapping("/view/create_category")
+    fun category(): String {
+        return "create_category"
+    }
+
+    @GetMapping("/view/create_product")
+    fun createProduct(): String {
+        return "create_product"
+    }
+
+    @GetMapping("/view/shop")
+    fun shop(): String {
+        return "shop"
+    }
+
 }
