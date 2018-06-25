99fa3e45cd8fc48fc60fa3385244b9ae92ab782c
seko0716 <sergey3440@gmail.com>
2018-06-25 12:09:32 +0400

    add repositories for user and Account

--- buisness/account/src/main/kotlin/net/sergey/kosov/account/repositories/AccountRepository.kt
+++ buisness/account/src/main/kotlin/net/sergey/kosov/account/repositories/AccountRepository.kt
@@ -1,5 +1,9 @@
 package net.sergey.kosov.account.repositories
 
+import net.sergey.kosov.account.domains.Account
+import net.sergey.kosov.common.repositories.RepositoryQuery
+import org.springframework.data.mongodb.repository.MongoRepository
+import org.springframework.stereotype.Repository
 
-interface AccountRepository {
-}
+@Repository
+interface AccountRepository : MongoRepository<Account, String>, RepositoryQuery<Account>
--- buisness/account/src/main/kotlin/net/sergey/kosov/account/repositories/UserRepository.kt
+++ buisness/account/src/main/kotlin/net/sergey/kosov/account/repositories/UserRepository.kt
@@ -1,5 +1,9 @@
 package net.sergey.kosov.account.repositories
 
+import net.sergey.kosov.account.domains.User
+import net.sergey.kosov.common.repositories.RepositoryQuery
+import org.springframework.data.mongodb.repository.MongoRepository
+import org.springframework.stereotype.Repository
 
-interface UserRepository {
-}
+@Repository
+interface UserRepository : MongoRepository<User, String>, RepositoryQuery<User>
