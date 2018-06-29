71e72af0ac5d99812ca404765eb76deea3ff90f9
seko0716 <sergey3440@gmail.com>
2018-06-29 12:33:56 +0400

    add implementation for service layer

---
+++ buisness/account/src/main/kotlin/net/sergey/kosov/account/services/AccountAccountService.kt
@@ -0,0 +1,19 @@
+package net.sergey.kosov.account.services
+
+import net.sergey.kosov.account.domains.Account
+import net.sergey.kosov.account.repositories.AccountRepository
+import net.sergey.kosov.common.exceptions.NotFoundException
+import org.springframework.data.mongodb.core.query.Criteria
+import org.springframework.data.mongodb.core.query.Query
+import org.springframework.stereotype.Service
+
+@Service
+class AccountAccountService(var accountRepository: AccountRepository) : AccountService {
+    override fun getAccount(name: String): Account {
+        val findByQuery = accountRepository.findByQuery(Query(Criteria.where("marketName").`is`(name)))
+        if (findByQuery.size != 1) {
+            throw NotFoundException("can not found account by marketName: $name")
+        }
+        return findByQuery.first()
+    }
+}
---
+++ buisness/account/src/main/kotlin/net/sergey/kosov/account/services/AccountUserService.kt
@@ -0,0 +1,19 @@
+package net.sergey.kosov.account.services
+
+import net.sergey.kosov.account.domains.User
+import net.sergey.kosov.account.repositories.UserRepository
+import net.sergey.kosov.common.exceptions.NotFoundException
+import org.springframework.data.mongodb.core.query.Criteria
+import org.springframework.data.mongodb.core.query.Query
+import org.springframework.stereotype.Service
+
+@Service
+class AccountUserService(var userRepository: UserRepository) : UserService {
+    override fun getUser(username: String): User {
+        val findByQuery = userRepository.findByQuery(Query(Criteria.where("name").`is`(username)))
+        if (findByQuery.size != 1) {
+            throw NotFoundException("can not found user by name: $username")
+        }
+        return findByQuery.first()
+    }
+}
