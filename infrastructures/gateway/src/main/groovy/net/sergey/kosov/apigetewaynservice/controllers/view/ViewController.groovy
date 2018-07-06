e8a6b6e4ecbdb16c9aa3776665a7cd67d6569b70
Sergey34 <sergey3440@gmail.com>
2018-07-06 22:12:23 +0400

    изменил конфигурацию на работу с инфраструктурными сервисами

---
+++ infrastructures/gateway/src/main/groovy/net/sergey/kosov/apigetewaynservice/controllers/view/ViewController.groovy
@@ -0,0 +1,48 @@
+package net.sergey.kosov.apigetewaynservice.controllers.view
+
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.GetMapping
+
+@Controller
+class ViewController {
+
+    @GetMapping("/view/products")
+    String products() {
+        return "products"
+    }
+
+    @GetMapping("/view/product/*")
+    String product() {
+        return "product"
+    }
+
+    @GetMapping("/view/create_category")
+    String category() {
+        return "create_category"
+    }
+
+    @GetMapping("/view/create_product")
+    String createProduct() {
+        return "create_product"
+    }
+
+    @GetMapping("/view/shop/*")
+    String shop() {
+        return "shop"
+    }
+
+    @GetMapping("/view/cart")
+    String cart() {
+        return "cart"
+    }
+
+    @GetMapping("/view/order/*")
+    String order() {
+        return "order"
+    }
+
+    @GetMapping("/view/orders")
+    String orders() {
+        return "orders"
+    }
+}
